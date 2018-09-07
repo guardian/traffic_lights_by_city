9ad3df9cc6656ca3fe49b45b1197ffcc1c361588
nickjevershed <nick.evershed@gmail.com>
2018-09-07 16:16:08 +1000

    more stopwatch

--- src/js/modules/traffic.js
+++ src/js/modules/traffic.js
@@ -34,15 +34,12 @@ var citylights = [{
 
 
 function displayTime(time) {
-	console.log(time)
 	var seconds = Math.trunc(time);
 	var milliseconds = time % 1
 	var minutes = seconds/60;
 	var displaySeconds = "00"
 	var displayMinutes = "00"
 	var displayMilliseconds = "00"
-
-	console.log(seconds, minutes)
 
 	if (minutes >= 1) {
 		seconds = seconds - (Math.floor(minutes) * 60)
@@ -56,16 +53,21 @@ function displayTime(time) {
 		displaySeconds = seconds
 	}
 
-	if (minutes > 1 && minutes < 10) {
-		displayMinutes = "0" + minutes.toFixed();
+	if (minutes >= 1 && minutes < 10) {
+		displayMinutes = "0" + Math.floor(minutes);
 	}
 
-	if (milliseconds > 0.95) {
-		milliseconds = 0;
+	if (milliseconds > 0.99) {
+		displayMilliseconds = "00"
 	}
 
-	displayMilliseconds = (milliseconds*10).toFixed();
-
+	else if (milliseconds <= 0.09) {
+		displayMilliseconds = "0" + (milliseconds*100).toFixed();
+	}
+	else {
+		displayMilliseconds = (milliseconds*100).toFixed();
+	}
+	
 	d3.select("#time .minutes").html(displayMinutes)
 	d3.select("#time .seconds").html(displaySeconds)
 	d3.select("#time .milliseconds").html(displayMilliseconds)
@@ -82,17 +84,39 @@ export class Traffic {
 
 		this.max = 150
 
+		var firstRun = true;
+
 		d3.select("#button").on("click", function() {
-			// d3.select(this).style("display", "none")
-			self.timer() 
+			if (firstRun) {
+				self.timer('start') 
+				firstRun = false	
+			}
+
+			else {
+				console.log('reset')
+				self.timer('reset') 
+			}
+			
 		})
 
 	}
 
 
-	timer() {
+	timer(status) {
 
 		var self = this
+
+		if (status == 'reset') {
+			clearTimeout(self.clock);
+			// t.stop()
+		  	d3.select("#time .minutes").html("00")
+			d3.select("#time .seconds").html("00")
+			d3.select("#time .milliseconds").html("00")
+			self.seconds = 0
+			d3.selectAll('.standing_man_svg').style('fill', 'red');
+			d3.selectAll('.walking_man_svg').style('fill', 'lightgrey');
+			d3.selectAll('.city_time_box').html("");
+		}
 
 		this.clock = setInterval(function(){ 
 
@@ -112,9 +136,11 @@ export class Traffic {
 			var display = (elapsed) / 1000
 			displayTime(display);
 			
-		  if (elapsed > 150000 / 2) { 
+		  if (elapsed > 150000) { 
 		  	t.stop()
-		  	// d3.select("#time").html(150.0)
+		  	d3.select("#time .minutes").html("02")
+			d3.select("#time .seconds").html("30")
+			d3.select("#time .milliseconds").html("0")
 		  };
 		}, 150);
 
@@ -136,7 +162,7 @@ export class Traffic {
 				d3.selectAll('.walking_man_svg')
 				  .each(function(d, index) {if (index===i) d3.select(this).style('fill', 'green')});
 
-				document.getElementsByClassName("city_time_box")[i].innerHTML = citylights[i].wait_time + ' seconds'
+				document.getElementsByClassName("city_time_box")[i].innerHTML = citylights[i].wait_time + ' sec'
 				
 			}
 			
